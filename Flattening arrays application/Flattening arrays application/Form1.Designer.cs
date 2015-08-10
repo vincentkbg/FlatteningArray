b8e90a2e5afb2a014122c81b15c951b8d403db49
Vincent Kabongo <vincent.kabongo@gmail.com>
2015-08-10 12:51:15 +0100

    Flattening Array

---
+++ Flattening arrays application/Flattening arrays application/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Flattening_arrays_application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nested array:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Flattened array:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(140, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Flattening nested arrays";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Flattening arrays application/Flattening arrays application/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Flattening_arrays_application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //initialisation of nested array
+            //the array is made of 4 inner arrays and all array are of type integers
+            int[][] nestedarray = new int[4][]
+            {
+                //inner arrays
+                new int [] {2,4,8,9},
+                new int [] {10,15,20,30,5,8,7,9},
+                new int [] {14,13,7,1,6,11},
+                new int [] {0,12,3,17}
+
+            };
+
+            //Display of elements of the nested array
+            //beginning of loop to display each element
+            for (int i = 0; i < nestedarray.Length; i++)
+            {
+                int[] innerArray = nestedarray[i];
+
+                for (int j = 0 ; j < innerArray.Length ; j++)
+                {
+                    label2.Text +=  nestedarray [i][j].ToString() + " ";
+                }
+
+            }
+
+            //Flattening array
+            //
+            //beginning of fltattening 
+            //each element from the nested is extracted from the nestedarray 
+            //each element is added to a new list
+            //the list is then converted to a one dimensional array
+            //each element of the one dimensional array is then printed
+            //the array is then displayed in the label4.
+
+            for (int i = 0; i < nestedarray.Length; i++)
+            {
+                int[] innerArray = nestedarray[i];
+
+                for (int j = 0; j < innerArray.Length; j++)
+                {
+                    var valuelist = new List<int>();
+                    valuelist.Add(nestedarray[i][j]);
+                    int[] flattenarray = valuelist.ToArray();
+
+                    for (int k = 0; k < flattenarray.Length; i++) 
+                    {
+                        label4.Text += flattenarray[k].ToString() + " ";
+
+                    }
+
+
+
+                    
+                }
+
+            }
+
+        }
+    }
+}
